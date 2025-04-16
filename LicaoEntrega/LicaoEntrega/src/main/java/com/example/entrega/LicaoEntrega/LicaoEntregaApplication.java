8dc2fcf893a63bf0f6ac9fe690411aa86c04dc74
IsaqueBorges01 <isaque.taioque@sptech.school>
2025-04-16 18:57:42 -0300

    estrutura do projeto

---
+++ LicaoEntrega/LicaoEntrega/src/main/java/com/example/entrega/LicaoEntrega/LicaoEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.example.entrega.LicaoEntrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicaoEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicaoEntregaApplication.class, args);
+	}
+
+}
---
+++ LicaoEntrega/LicaoEntrega/src/test/java/com/example/entrega/LicaoEntrega/LicaoEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entrega.LicaoEntrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicaoEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
